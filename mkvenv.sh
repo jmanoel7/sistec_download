98b1d2526438a6c9ced4ea02af491482dc120cb9
Joao Nogueira <joao.nogueira@ifg.edu.br>
2023-09-06 20:27:48 -0300

    add mkvenv.sh ...

---
+++ mkvenv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+mkdir -p ${HOME}/.local/venv/
+virtualenv -p /usr/bin/python3 ${HOME}/.local/venv/sistec_download_automatico
+source ${HOME}/.local/venv/sistec_download_automatico/bin/activate
+pip install -U pip
+pip install -U -r ${PWD}/requirements.txt
+exit 0
