f3101156c452d19fca04c0754e0d12277eb4c93b
Joao Manoel <joaomanoel7.dev@gmail.com>
2023-09-15 03:12:48 -0300

    fix bugs in browser.py

--- browser.py
+++ browser.py
@@ -10,16 +10,8 @@ BROWSER = None
 
 def start_browser():
     options = Options()
-    options.binary_location = '/usr/bin/firefox-esr' # <-- change here firefox binary location
-    # sudo apt-get install --yes firefox-esr
-    # create profile 'developer-esr' with command 'firefox-esr -P'
-    # init firefox-esr and choice profile 'developer-esr'
-    # now open url 'about:config' and set config:
-    # 1) moz:debuggerAddress -> true
-    # 2) fission.bfcacheInParent -> false
-    # 3) fission.webContentIsolationStrategy -> 0
-    options.add_argument('--safe-mode') # <-- start in safe-mode (disables extensions and themes for this session)
-    options.add_argument('-P developer-esr') # <-- choice created profile 'developer-esr'
+    options.binary_location = '/usr/local/bin/firefox' # <-- change here firefox binary location
+    options.add_argument('--safe-mode')
     service = Service(executable_path='/usr/local/bin/geckodriver') # <-- change here geckodriver binary location
     browser = webdriver.Firefox(options=options, service=service)
     return browser
