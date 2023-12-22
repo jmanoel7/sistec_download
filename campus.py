802cd0a828f553227e047e562c5e8852252a08d6
Joao Manoel <joao.nogueira@ifg.edu.br>
2023-12-22 10:46:12 -0300

    to back the code

--- campus.py
+++ campus.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 
 
+campus_spreadsheet_dict = {
+    u'CÂMPUS ÁGUAS LINDAS.csv': 3647,
+    u'CÂMPUS ANÁPOLIS.csv': 699,
+    u'CÂMPUS APARECIDA DE GOIÂNIA.csv': 210,
+    u'CÂMPUS CIDADE DE GOIÁS.csv': 696,
+    u'CÂMPUS FORMOSA.csv': 2012,
+    u'CÂMPUS GOIÂNIA.csv': 212,
+    u'CÂMPUS GOIÂNIA OESTE.csv': 3646,
+    u'CÂMPUS INHUMAS.csv': 238,
+    u'CÂMPUS ITUMBIARA.csv': 244,
+    u'CÂMPUS JATAÍ.csv': 241,
+    u'CÂMPUS LUZIÂNIA.csv': 209,
+    u'CÂMPUS SENADOR CANEDO.csv': 3648,
+    u'CÂMPUS URUAÇU.csv': 161,
+    u'CÂMPUS VALPARAÍSO.csv': 3649
+}
+
+
 campus_list = [
     u'CÂMPUS ÁGUAS LINDAS',
     u'CÂMPUS ANÁPOLIS',
@@ -17,4 +35,5 @@ campus_list = [
     u'CÂMPUS URUAÇU',
     u'CÂMPUS VALPARAÍSO'
 ]
+
 
