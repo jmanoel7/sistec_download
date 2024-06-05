5343d711d9e0293e4838f74eefc26849f5a7bbf2
Joao Manoel <joaomanoel7.dev@gmail.com>
2024-06-05 20:28:37 -0300

    bug fix: remove comments

--- main.sh
+++ main.sh
@@ -22,71 +22,71 @@ export FILE_EAD_TWO_CODE_OUT="${DIR_BASE}/codes_ead_two_sort.txt"
 export GIT_PROJECT_PATH="${HOME}/git/ifg/sistec_download"
 
 
-# # clean and mkdir dir_base
-# rm -rf "$DIR_BASE"
-# mkdir -p "$DIR_BASE"
-#
-#
-# # clean sistec_csv* and cursos_tecnicos* from user's home
-# rm -f ~/Downloads/sistec_csv*
-# rm -f ~/Downloads/cursos_tecnicos*
-#
-#
-# # create folders for course codes to all campi
-# python ./create_folders_course_codes.py
-#
-#
-# echo -e '\nPronto! Agora você já pode baixar as planilhas com os códigos dos cursos direto do sistec!'
-# echo -e 'Baixe cada planilha e mova-a para a sua devida pasta (de acordo com o nome do campus).'
-# echo -e "A raiz onde as pastas (campus) ficam é: ${DIR_BASE} ."
-#
-# opt="0"
-# while [[ "$opt" != "s" ]] && [[ "$opt" != "S" ]] && [[ "$opt" != "n" ]] && [[ "$opt" != "N" ]]
-# do
-#     echo -e '\nJá baixou as TODAS as planilhas e as colocou em suas devidas pastas? (S/N)?'
-#     read opt
-# done
-#
-# if [[ "$opt" = "N" ]] || [[ "$opt" = "n" ]]
-# then
-#     echo "Não se esqueça de continuar o código de onde parou se for o caso,"
-#     echo "aí altere momentaneamente o arquivo campus.py e depois restaure-o com o all_campus.py !"
-#     exit 0
-# fi
-#
-#
-# python make list course codes from spreadsheets csv
-# python ./make-list-course-codes.py
+# clean and mkdir dir_base
+rm -rf "$DIR_BASE"
+mkdir -p "$DIR_BASE"
 
 
-# sort course codes
-#sort -n -u $FILE_CODE_IN > $FILE_CODE_OUT
-#sort -n -u $FILE_EAD_CODE_IN > $FILE_EAD_CODE_OUT
+# clean sistec_csv* and cursos_tecnicos* from user's home
+rm -f ~/Downloads/sistec_csv*
+rm -f ~/Downloads/cursos_tecnicos*
 
 
-# separation of distance course codes
-#python ./separation-course-codes.py
+# create folders for course codes to all campi
+python ./create_folders_course_codes.py
 
 
-# delete lines with old course codes in tweak_spreadsheets.py
-#sed -i '/\# -----BEGIN DISTANCE COURSE CODES-----/,/\# -----END DISTANCE COURSE CODES-----/ { /\# -----BEGIN DISTANCE COURSE CODES-----/ b; /\# -----END DISTANCE COURSE CODES-----/ b; d }' tweak_spreadsheets.py
-#sed -i '/\# -----BEGIN FACE-TO-FACE COURSE CODES-----/,/\# -----END FACE-TO-FACE COURSE CODES-----/ { /\# -----BEGIN FACE-TO-FACE COURSE CODES-----/ b; /\# -----END FACE-TO-FACE COURSE CODES-----/ b; d }' tweak_spreadsheets.py
+echo -e '\nPronto! Agora você já pode baixar as planilhas com os códigos dos cursos direto do sistec!'
+echo -e 'Baixe cada planilha e mova-a para a sua devida pasta (de acordo com o nome do campus).'
+echo -e "A raiz onde as pastas (campus) ficam é: ${DIR_BASE} ."
+
+opt="0"
+while [[ "$opt" != "s" ]] && [[ "$opt" != "S" ]] && [[ "$opt" != "n" ]] && [[ "$opt" != "N" ]]
+do
+    echo -e '\nJá baixou as TODAS as planilhas e as colocou em suas devidas pastas? (S/N)?'
+    read opt
+done
+
+if [[ "$opt" = "N" ]] || [[ "$opt" = "n" ]]
+then
+    echo "Não se esqueça de continuar o código de onde parou se for o caso,"
+    echo "aí altere momentaneamente o arquivo campus.py e depois restaure-o com o all_campus.py !"
+    exit 0
+fi
 
 
-# insert lines with new course codes in tweak_spreadsheets.py
-#./make-lines-course-codes.sh 16 $FILE_EAD_TWO_CODE_OUT | \
-#    sed -i '/\# -----BEGIN DISTANCE COURSE CODES-----/,/\# -----END DISTANCE COURSE CODES-----/ r /dev/stdin' tweak_spreadsheets.py
-#./make-lines-course-codes.sh 16 $FILE_CODE_OUT | \
-#    sed -i '/\# -----BEGIN FACE-TO-FACE COURSE CODES-----/,/\# -----END FACE-TO-FACE COURSE CODES-----/ r /dev/stdin' tweak_spreadsheets.py
-#
-#
-## delete lines with old course codes in tweak_ead_spreadsheets.py
-#sed -i '/\# -----BEGIN DISTANCE COURSE CODES-----/,/\# -----END DISTANCE COURSE CODES-----/ { /\# -----BEGIN DISTANCE COURSE CODES-----/ b; /\# -----END DISTANCE COURSE CODES-----/ b; d }' tweak_ead_spreadsheets.py
-#
-#
-## insert lines with new course codes in tweak_ead_spreadsheets.py
-#./make-lines-course-codes.sh 12 $FILE_EAD_ONE_CODE_OUT | \
-#    sed -i '/\# -----BEGIN DISTANCE COURSE CODES-----/,/\# -----END DISTANCE COURSE CODES-----/ r /dev/stdin' tweak_ead_spreadsheets.py
+python make list course codes from spreadsheets csv
+python ./make-list-course-codes.py
+
+
+sort course codes
+sort -n -u $FILE_CODE_IN > $FILE_CODE_OUT
+sort -n -u $FILE_EAD_CODE_IN > $FILE_EAD_CODE_OUT
+
+
+separation of distance course codes
+python ./separation-course-codes.py
+
+
+delete lines with old course codes in tweak_spreadsheets.py
+sed -i '/\# -----BEGIN DISTANCE COURSE CODES-----/,/\# -----END DISTANCE COURSE CODES-----/ { /\# -----BEGIN DISTANCE COURSE CODES-----/ b; /\# -----END DISTANCE COURSE CODES-----/ b; d }' tweak_spreadsheets.py
+sed -i '/\# -----BEGIN FACE-TO-FACE COURSE CODES-----/,/\# -----END FACE-TO-FACE COURSE CODES-----/ { /\# -----BEGIN FACE-TO-FACE COURSE CODES-----/ b; /\# -----END FACE-TO-FACE COURSE CODES-----/ b; d }' tweak_spreadsheets.py
+
+
+insert lines with new course codes in tweak_spreadsheets.py
+./make-lines-course-codes.sh 16 $FILE_EAD_TWO_CODE_OUT | \
+   sed -i '/\# -----BEGIN DISTANCE COURSE CODES-----/,/\# -----END DISTANCE COURSE CODES-----/ r /dev/stdin' tweak_spreadsheets.py
+./make-lines-course-codes.sh 16 $FILE_CODE_OUT | \
+   sed -i '/\# -----BEGIN FACE-TO-FACE COURSE CODES-----/,/\# -----END FACE-TO-FACE COURSE CODES-----/ r /dev/stdin' tweak_spreadsheets.py
+
+
+# delete lines with old course codes in tweak_ead_spreadsheets.py
+sed -i '/\# -----BEGIN DISTANCE COURSE CODES-----/,/\# -----END DISTANCE COURSE CODES-----/ { /\# -----BEGIN DISTANCE COURSE CODES-----/ b; /\# -----END DISTANCE COURSE CODES-----/ b; d }' tweak_ead_spreadsheets.py
+
+
+# insert lines with new course codes in tweak_ead_spreadsheets.py
+./make-lines-course-codes.sh 12 $FILE_EAD_ONE_CODE_OUT | \
+   sed -i '/\# -----BEGIN DISTANCE COURSE CODES-----/,/\# -----END DISTANCE COURSE CODES-----/ r /dev/stdin' tweak_ead_spreadsheets.py
 
 
 # clean and mkdir dir_audit
@@ -136,4 +136,3 @@ cd ../..
 
 
 exit 0
-
